158b520cfae0d81f2d27b90748dcdb1228705f32
Achintya Tiwary <121821415+achintw@users.noreply.github.com>
2024-04-16 13:04:39 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import {lazy} from 'react'
 import Loader from "./components/loader"
 
-
-// import Dashboard from './pages/dashboard'
-// instead of this ^ we will load it lazy 
-
+// lazy loading
 const Assgn = lazy(()=>import("./pages/assgn"))
 
 function App() {
@@ -15,13 +12,6 @@ function App() {
       <Suspense fallback={<Loader/>}>
       <Routes>
         <Route path="/admin/assgn" element={<Assgn/>}/>
-
-        {/* Charts */}
-
-
-        {/* Apps */}
-
-        
       </Routes>
       </Suspense>
     </Router>
